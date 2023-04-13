1fd0a01ad35c4e599ab45f9cc94471490dc41026
osaka <sensh0god@gmail.com>
2023-04-13 18:05:19 +0300

    made example for number 23 from kege

--- inf/numbers_1.py
+++ inf/numbers_1.py
@@ -313,6 +313,23 @@ for a in range(100):
         if game(a,b):
             if abs(a-b)<= minimal:
                 minimal = abs(a-b)
-                print(a,b)"""      
+                print(a,b)"""    
+
+
+
+'''def  f(a,b,m=0):            #номер 23 из  кегэ
+    if a > b:
+        return 0
+    if a == b:
+        if m <= 15:
+            return 1 
+        else:
+            return 0 
+    if a % 2 == 0:
+        return f(a+2,b,m+1) + f(a+3,b,m+1) + f(a*2+1,b,m+1)
+    if a % 2 != 0:
+        return f(a+2,b,m) + f(a+3,b,m) + f(a*2+1,b,m)
+    
+print(f(1,55))'''
 
         

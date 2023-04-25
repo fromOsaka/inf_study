79836c073c98a13cc248e7ae881465bb0f1ab1f7
osaka <sensh0god@gmail.com>
2023-04-25 19:20:43 +0300

    make some changes

--- inf/practics.py
+++ inf/practics.py
@@ -1,8 +1,12 @@
-def f(a,b):
-    if a == b:
-        return 1
-    if a > b:
-        return 0
-    return f(a+1,b) + f(a*3,b) + f(a+2,b)
-
-print(f(2,9) * f(9,11) * f(11,12))
+text = open('244-2.txt').read()
+statis = {}
+for i in range(2,len(text)-1):
+    if text[i-2] == text[i-1]:
+        if text[i] not in statis:
+            statis[text[i]] = 1
+        else:
+            statis[text[i]] += 1
+print(statis)
+result = [(statis[letter],letter) for letter in statis]
+result.sort()
+print(result)

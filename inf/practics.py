9967bd29a95c01de34c29695785ad4cd052b0317
osaka <sensh0god@gmail.com>
2023-04-29 13:26:43 +0300

    new example nO 24 in prac

--- inf/practics.py
+++ inf/practics.py
@@ -1,12 +1,12 @@
-text = open('244-2.txt').read()
-statis = {}
-for i in range(2,len(text)-1):
-    if text[i-2] == text[i-1]:
-        if text[i] not in statis:
-            statis[text[i]] = 1
-        else:
-            statis[text[i]] += 1
-print(statis)
-result = [(statis[letter],letter) for letter in statis]
-result.sort()
-print(result)
+file = open('107_24.txt').read()
+file = file.replace("AB", 'x')
+file = file.replace("CB",'x')
+k = 0
+m = 0
+for i in range(len(file)):
+    if file[i] == 'x':
+        k += 1
+        m = max(m,k)
+    else:
+        k = 0
+print(m)
